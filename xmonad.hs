2e5df3656f7d5516d6c08dfce361fcaa1d653a2e
Joe Gomain Hoyes <gomain@gmail.com>
2020-11-19 11:42:25 +0700

    remove default key binding (modm, xK_l)

--- xmonad.hs
+++ xmonad.hs
@@ -1,4 +1,4 @@
-import qualified Data.Map as M
+import qualified Data.Map as Map
 import Data.Map (Map)
 import           XMonad
 import qualified XMonad.StackSet as W
@@ -13,7 +13,7 @@ main = do
   xmonad $ def {
     layoutHook =  boringWindows $ tall ||| Full
   , modMask = windowsMask -- altMask .|. controlMask
-  , keys = \conf -> myKeys conf `M.union` keys def conf -- M.union is left biased
+  , keys = myKeys
   , startupHook = do
       spawn "xmodmap ~/.Xmodmap"
   }
@@ -28,36 +28,44 @@ main = do
     , _slaves  = [ 2 ]   -- fraction to multiply to slave window heights
     }
     myKeys :: XConfig Layout -> Map (ButtonMask, KeySym) (X ())
-    myKeys conf@(XConfig {
-      X.modMask    = modm
-    , X.workspaces = workspaces
-    }) = M.fromList $ [
-        ((modm, xK_r),     refresh)
-      , ((modm, xK_d),     sendMessage Shrink)
-      , ((modm, xK_minus), sendMessage Expand)
-      , ((modm, xK_s),     sendMessage MirrorShrink)
-      , ((modm, xK_h),     sendMessage MirrorExpand)
-      ] ++ [
-        ((modm .|. mask, xK_n), f)
-          | (mask, f) <- [ (noMask, focusDown), (shiftMask, windows W.swapDown) ]
-      ] ++ [
-        ((modm .|. mask, xK_t), f)
-          | (mask, f) <- [ (noMask, focusUp), (shiftMask, windows W.swapUp) ]
-      ] ++ [
-        ((modm .|. mask, key), screenWorkspace scrId >>= flip whenJust (windows . f))
-          | (scrId, key) <- zip screens [xK_a, xK_o, xK_e, xK_u]
-          , (mask, f)    <- [(noMask, W.view), (shiftMask, W.shift)]
-      ] ++ [
-        ((modm .|. mask, key), windows $ f wspId)
-          | (wspId, key) <- zip workspaces [xK_1 ..]
-          , (mask, f)    <- [
-              (noMask,      W.greedyView)
-            , (shiftMask,   W.shift)
-            , (controlMask, swapWithCurrent)
-            ]
-      ] ++ [
-        ((modm, xK_0), withFocused $ windows . W.sink )
-      ]
+    -- Data.Map.union is left biased
+    myKeys conf = myOverrides conf `Map.union` foldr Map.delete (keys def conf) (myRemoves conf)
       where
-        screens = [0..]
-  
+        myOverrides :: XConfig Layout -> Map (ButtonMask, KeySym) (X ())
+        myOverrides XConfig {
+          X.modMask    = modm
+        , X.workspaces = workspaces
+        } = Map.fromList $ [
+            ((modm, xK_r),     refresh)
+          , ((modm, xK_d),     sendMessage Shrink)
+          , ((modm, xK_minus), sendMessage Expand)
+          , ((modm, xK_s),     sendMessage MirrorShrink)
+          , ((modm, xK_h),     sendMessage MirrorExpand)
+          ] ++ [
+            ((modm .|. mask, xK_n), f)
+              | (mask, f) <- [ (noMask, focusDown), (shiftMask, windows W.swapDown) ]
+          ] ++ [
+            ((modm .|. mask, xK_t), f)
+              | (mask, f) <- [ (noMask, focusUp), (shiftMask, windows W.swapUp) ]
+          ] ++ [
+            ((modm .|. mask, key), screenWorkspace scrId >>= flip whenJust (windows . f))
+              | (scrId, key) <- zip screens [xK_a, xK_o, xK_e, xK_u]
+              , (mask, f)    <- [(noMask, W.view), (shiftMask, W.shift)]
+          ] ++ [
+            ((modm .|. mask, key), windows $ f wspId)
+              | (wspId, key) <- zip workspaces [xK_1 ..]
+              , (mask, f)    <- [
+                  (noMask,      W.greedyView)
+                , (shiftMask,   W.shift)
+                , (controlMask, swapWithCurrent)
+                ]
+          ] ++ [
+            ((modm, xK_0), withFocused $ windows . W.sink )
+          ]
+          where
+            screens = [0..]
+        myRemoves :: XConfig Layout -> [(ButtonMask, KeySym)]
+        myRemoves XConfig { X.modMask = modm } = [
+            -- remove default key for `sendMessage MirrorShrink`
+            (modm, xK_l)
+          ]
